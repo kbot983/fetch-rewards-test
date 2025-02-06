e54ec507553bcea747b64be4593f1de58b705895
Karan Botadra <kbot983@gmail.com>
2025-02-06 13:35:54 -0600

    Add authentication check to matches page loader

--- src/functions/matchesPage.ts
+++ src/functions/matchesPage.ts
@@ -1,8 +1,12 @@
 import api from "@/lib/api";
 import reduxStore from "@/store/index";
 import { Match, Dog } from "@/types";
+import { redirect } from "react-router-dom";
 
 export const loader = async () => {
+  if (!reduxStore.getState().auth.isAuthenticated) {
+    return redirect("/login");
+  }
   const favoriteIds = reduxStore.getState().favorite.favoriteIds;
   try {
     const response = await api.post<Match>("/dogs/match", favoriteIds);
